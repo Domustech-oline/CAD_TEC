1ecc4f61a5f7629e9d8add2d8293f3d50f4da691
C. Abreu Souza <inhokarl90@gmail.com>
2026-07-05 16:49:22 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,65 @@
+const CACHE="solarflow-cadtec-v4"
+
+const FILES=[
+"./",
+"./index.html",
+"./style.css",
+"./app.js",
+"./manifest.webmanifest",
+"./assets/logo.png",
+"./icons/icon-192.png",
+"./icons/icon-512.png",
+"./icons/icon-192-maskable.png",
+"./icons/icon-512-maskable.png"
+]
+
+self.addEventListener("install",e=>{
+
+// Ativa a nova versão imediatamente, sem esperar todas as abas fecharem
+self.skipWaiting()
+
+e.waitUntil(
+
+caches.open(CACHE).then(cache=>
+
+// cacheia cada arquivo individualmente: se um falhar, os outros continuam
+Promise.all(
+FILES.map(url=>cache.add(url).catch(err=>console.warn("Falha ao cachear",url,err)))
+)
+
+)
+
+)
+
+})
+
+self.addEventListener("activate",e=>{
+
+e.waitUntil(
+
+(async()=>{
+
+const keys=await caches.keys()
+
+await Promise.all(
+keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))
+)
+
+// assume o controle das páginas abertas imediatamente
+await self.clients.claim()
+
+})()
+
+)
+
+})
+
+self.addEventListener("fetch",e=>{
+
+e.respondWith(
+
+caches.match(e.request).then(res=>res||fetch(e.request))
+
+)
+
+})
